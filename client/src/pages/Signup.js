9934ea0a61b55c40e07011eed39179236796d5a8
Liquid Monks <info@liquidmonks.com>
2023-04-03 17:07:15 -0500

    client/src/pages/Signup.js modifies the className attribute with tailwind styling css.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import AddToCart from '../assets/add_to_cart.svg'
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
-  const [addUser] = useMutation(ADD_USER);
+  const [addUser, { error, loading }] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -31,55 +32,69 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <div className="container mx-auto px-2 py-5">
+      <Link to="/login" className='link'>← Go to Login</Link>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
+      <div className='grid grid-cols-2 h-full gap-4 mt-10'>
+        <div className='col-span-2 lg:col-span-1'>
+          <h2>Signup</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                placeholder="First"
+                name="firstName"
+                type="firstName"
+                id="firstName"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                placeholder="Last"
+                name="lastName"
+                type="lastName"
+                id="lastName"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            {error ? (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            ) : null}
+            <button disabled={loading} type="submit" className='btn-primary w-full'>Submit</button>
+          </form>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
+        <div className='hidden lg:block'>
+          <img src={AddToCart} alt='web shopping' />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
