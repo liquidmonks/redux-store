1390ebe9626d50140bea7b2429d6cbd5d50481e8
Liquid Monks <info@liquidmonks.com>
2023-04-03 15:28:12 -0500

    client/src/index.js sets up the react app with all neccessary imports for the application.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
+// Import React and other necessary modules
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(
+// Get the root element from the HTML document
+const rootElement = document.getElementById('root');
+// Create a root for rendering the React components
+const root = createRoot(rootElement);
+
+// Render the App component within the React.StrictMode
+// StrictMode is a tool for highlighting potential problems in an application
+// It helps to identify unsafe lifecycle methods, legacy context API usage, and more
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Register the service worker to enable offline functionality and faster loading
+// Note that using service workers can introduce some pitfalls
+// Read more about service workers and Progressive Web Apps (PWA) at the provided link
+// https://bit.ly/CRA-PWA
 serviceWorker.register();
