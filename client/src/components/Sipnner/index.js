80348296abea2107b65505663b9a6d7562a9b501
Liquid Monks <info@liquidmonks.com>
2023-04-03 17:16:56 -0500

    client/src/components includes a spinner directory with an index.js effect script with tailwindcss styling to the application.

---
+++ client/src/components/Sipnner/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div className='w-full h-full min-h-[70vh] flex items-center justify-center'>
+        <span class="loader"></span>
+    </div>
+  )
+}
