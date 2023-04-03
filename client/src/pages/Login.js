f64874aeddb23b6b3a342e33557bae020309cb58
Liquid Monks <info@liquidmonks.com>
2023-04-03 17:11:24 -0500

    client/src/pages/Login.js modifies the script with tailwindcss styling properties.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+import WebShopingSvg from '../assets/web_shopping.svg'
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -30,40 +31,47 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+    <div className="container mx-auto px-2 py-5 min-h-[100vh - 60px]">
+      <Link to="/signup" className='link'>← Go to Signup</Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+      <div className='grid grid-cols-2 h-full gap-4 mt-10'>
+        <div className='hidden lg:block'>
+          <img src={WebShopingSvg} alt='web shopping' />
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
+        <div className='col-span-2 lg:col-span-1'>
+          <h2>Login</h2>
+          <form onSubmit={handleFormSubmit} className='rounded-md px-3 py-5'>
+            <div className="mb-3">
+              <label htmlFor="email">Email address:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+                className='input-primary w-full'
+              />
+            </div>
+            {error ? (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            ) : null}
+            <button className='btn-primary w-full' type="submit">Submit</button>
+          </form>
         </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
