b3996914a27974f7bdebab67f9d15c043197d882
Liquid Monks <info@liquidmonks.com>
2023-04-03 17:12:12 -0500

    client/src/pages/Home.js modifies the script with tailwindcss styling properties.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ import Cart from "../components/Cart";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="container mx-auto px-2 py-5">
       <CategoryMenu />
       <ProductList />
       <Cart />
