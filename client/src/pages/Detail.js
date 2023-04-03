2618ddd02cb4ad21b2787818195c0da6e3660e0e
Liquid Monks <info@liquidmonks.com>
2023-04-03 17:13:26 -0500

    client/src/pages/Detail.js modifies the script with tailwindcss styling properties.

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,26 +3,23 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
-import spinner from '../assets/spinner.gif';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart, updateCartQuantity } from '../redux/cartSlice'
+import { updateProducts } from '../redux/productsSlice'
+import Spinner from '../components/Sipnner';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products, cart } = state;
+  const products = useSelector(state => state.products.products)
+  const cart = useSelector(state => state.cart.cart)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     // already in global store
@@ -31,10 +28,7 @@ function Detail() {
     }
     // retrieved from server
     else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+      dispatch(updateProducts(data.products))
 
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
@@ -43,72 +37,70 @@ function Detail() {
     // get cache from idb
     else if (!loading) {
       idbPromise('products', 'get').then((indexedProducts) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: indexedProducts,
-        });
+        dispatch(updateProducts(data.indexedProducts))
       });
     }
   }, [products, data, loading, dispatch, id]);
 
-  const addToCart = () => {
+  const handleaddToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
-      });
+      dispatch(updateCartQuantity({
+        productId: id,
+        quantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      }))
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 },
-      });
+      dispatch(addToCart({ ...currentProduct, purchaseQuantity: 1 }))
       idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
     }
   };
 
-  const removeFromCart = () => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: currentProduct._id,
-    });
+  const handleRemoveFromCart = () => {
+    dispatch(removeFromCart(currentProduct._id))
 
     idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   return (
     <>
-      {currentProduct && cart ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+      {currentProduct && loading ? <Spinner /> :
+        cart ? (
+          <div className="container mx-auto px-2 py-5">
+            <Link to="/" className='link'>← Back to Products</Link>
 
-          <h2>{currentProduct.name}</h2>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 mt-10'>
+              <img
+                src={`/images/${currentProduct.image}`}
+                alt={currentProduct.name}
+                className='max-w-full rounded-lg'
+              />
+              <div className='lg:col-span-2'>
+                <h2>{currentProduct.name}</h2>
 
-          <p>{currentProduct.description}</p>
+                <p>{currentProduct.description}</p>
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
-              Remove from Cart
-            </button>
-          </p>
+                <span className='text-lg mb-5'>
+                  <strong>Price:</strong>${currentProduct.price}
+                </span>
+                <div className='flex items-center flex-wrap gap-3 mb-5'>
+                  <button className='btn-primary' onClick={handleaddToCart}>Add to Cart</button>
+                  <button
+                    className='btn-primary'
+                    disabled={!cart.find((p) => p._id === currentProduct._id)}
+                    onClick={handleRemoveFromCart}
+                  >
+                    Remove from Cart
+                  </button>
+                </div>
+              </div>
+            </div>
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
+          </div>
+        ) : null}
       <Cart />
     </>
   );
