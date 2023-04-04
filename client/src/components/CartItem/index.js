9490d77ff3c6ece5e12d705a1acba58902d2ff3c
Liquid Monks <info@liquidmonks.com>
2023-04-04 12:11:52 -0500

    client/src/components/CartItem/index imports redux and tailwindcss styling.

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,66 +1,57 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { removeFromCart, updateCartQuantity } from '../../redux/cartSlice'
+import { useDispatch } from 'react-redux';
+import { TrashIcon } from '@heroicons/react/24/solid'
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch()
 
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
+  const handleRemoveFromCart = item => {
+    dispatch(removeFromCart(item._id))
     idbPromise('cart', 'delete', { ...item });
-
   };
 
   const onChange = (e) => {
     const value = e.target.value;
     if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
+      dispatch(removeFromCart(item._id))
       idbPromise('cart', 'delete', { ...item });
 
     } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
+      dispatch(updateCartQuantity({
+        productId: item._id,
+        quantity: parseInt(value)
+      }))
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
 
     }
   }
 
   return (
-    <div className="flex-row">
-      <div>
-        <img
-          src={`/images/${item.image}`}
-          alt=""
-        />
-      </div>
-      <div>
-        <div>{item.name}, ${item.price}</div>
-        <div>
-          <span>Qty:</span>
+    <div className="grid grid-cols-3 gap-3">
+      <img
+        src={`/images/${item.image}`}
+        alt=""
+        className='rounded'
+      />
+      <div className='col-span-2 flex flex-col justify-center'>
+        <span className='text-sm'>{item.name}, ${item.price}</span>
+        <div className='flex gap-2'>
           <input
             type="number"
             placeholder="1"
             value={item.purchaseQuantity}
             onChange={onChange}
+            className='input-primary-sm'
           />
-          <span
-            role="img"
+          <button
             aria-label="trash"
-            onClick={() => removeFromCart(item)}
+            onClick={() => handleRemoveFromCart(item)}
           >
-            🗑️
-          </span>
+            <TrashIcon className='w-5 h-5 text-amber-500' />
+          </button>
         </div>
       </div>
     </div>
