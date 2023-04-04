fb8efbfce2d7869329c908f9f9431b4182b2a28b
Liquid Monks <info@liquidmonks.com>
2023-04-04 12:07:50 -0500

    client/src/components/Nave/index updates icons and tailwindcss styling.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,58 +1,54 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import AMAZON_LOGO from '../../assets/amazon-logo.webp'
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline'
 
 function Nav() {
-
-  function showNavigation() {
-    if (Auth.loggedIn()) {
-      return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
-          </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
-      );
-    }
-  }
-
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+    <header className="p-2 bg-eerie text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to="/" className="mr-3">
+          <img src={AMAZON_LOGO} className='max-w-[3rem] rounded-md' alt='logo' />
         </Link>
-      </h1>
 
-      <nav>
-        {showNavigation()}
-      </nav>
+        <nav>
+          <ul className="flex items-center gap-2">
+            {
+              Auth.loggedIn() ? <>
+                <li className="mx-1">
+                  <Link to="/orderHistory">
+                    Order History
+                  </Link>
+                </li>
+                <li className="mx-1">
+                  {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+                  <button onClick={() => Auth.logout()} className='flex items-center gap-1'>
+                    <span>Logout</span>
+                    <ArrowRightOnRectangleIcon className="w-5 h-5" />
+                  </button>
+                </li>
+              </>
+                : <>
+                  <li className="mx-1">
+                    <Link to="/signup">
+                      Signup
+                    </Link>
+                  </li>
+                  <li className="mx-1">
+                    <Link to="/login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+            }
+          </ul>
+        </nav>
+      </div>
     </header>
-  );
+  )
+
+
 }
 
 export default Nav;
