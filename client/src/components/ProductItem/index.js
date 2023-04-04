445d25e9f1493a95339f6410ea2a8851653d679c
Liquid Monks <info@liquidmonks.com>
2023-04-04 12:05:09 -0500

    client/src/components/ProductItem/index updates cartquantity and tailwindcss styling.

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,58 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, updateCartQuantity } from '../../redux/cartSlice'
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  const { image, name, _id, price, quantity, canAdd = true } = item;
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  const cart = useSelector(state => state.cart.cart)
+  const dispatch = useDispatch()
 
-  const { cart } = state
-
-  const addToCart = () => {
+  const handleAddToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
+      dispatch(updateCartQuantity({
+        productId: _id,
+        quantity: parseInt(itemInCart.purchaseQuantity) + 1
+      }))
       idbPromise('cart', 'put', {
         ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
-      dispatch({
-        type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
-      });
+      dispatch(addToCart({ ...item, purchaseQuantity: 1 }))
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
 
   return (
-    <div className="card px-1 py-1">
-      <Link to={`/products/${_id}`}>
+    <div className="shadow-sm border rounded-md max-w-[20rem]">
+      <Link to={`/products/${_id}`} className='group'>
         <img
           alt={name}
           src={`/images/${image}`}
         />
-        <p>{name}</p>
+        <p className="px-2 mt-3 group-hover:underline group-hover:text-amber-500">{name}</p>
       </Link>
-      <div>
+      <div className="px-2 mb-2 flex flex-col">
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
+        {
+          canAdd ? <button className="btn-primary flex-grow-0 w-fit text-sm mt-4" onClick={handleAddToCart}>Add to cart</button>
+            : null
+        }
       </div>
-      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
